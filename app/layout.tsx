444e677b78410aad16208681fb6a94e0231f021a
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-04-17 21:39:01 +0530

    feat: ui fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,62 @@
-import { ClarityScript } from "@/components/clarity-script";
-import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
-import { Inter, Geist } from "next/font/google";
-import Link from "next/link";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import { Geist, Lora, Fira_Code } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const fontSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSerif = Lora({
+  subsets: ["latin"],
+  variable: "--font-serif",
+});
 
+const fontMono = Fira_Code({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
 export const metadata = {
-    title: "Screen Share - Share Your Screen Instantly",
-    description: "Share your screen instantly with anyone using a simple room code. No downloads or sign-ups required.",
-    keywords: ["screen sharing", "webrtc", "online screen share", "browser screen sharing", "free screen sharing", "share your screen", "share screen", "screen share"],
-    other: {
-        "google-site-verification": process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || ""
-    }
+  title: "Screen Share - Share Your Screen Instantly",
+  description:
+    "Share your screen instantly with anyone using a simple room code. No downloads or sign-ups required.",
+  keywords: [
+    "screen sharing",
+    "webrtc",
+    "online screen share",
+    "browser screen sharing",
+    "free screen sharing",
+    "share your screen",
+    "share screen",
+    "screen share",
+  ],
+  // other: {
+  //     "google-site-verification": process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || ""
+  // }
 } satisfies Metadata;
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="en" suppressHydrationWarning className={cn("font-sans", geist.variable)}>
-            <body className={inter.className}>
-                <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-                    <main>
-                        {children}
-                        {/* <footer className="text-muted-foreground px-4 py-8 text-center text-sm">
-                        The source code is available on{" "}
-                        <Link href="https://github.com/VA5UDEV/screen-sharing" className="underline" target="_blank">
-                            Github
-                        </Link>
-                        .
-                    </footer> */}
-                    </main>
-                    <ClarityScript />
-                    <Toaster richColors />
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${fontSans.variable} ${fontSerif.variable} ${fontMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main>{children}</main>
+          <Toaster richColors />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
